959aa1bc09fca0f6472985dba5099f4e23f8b622
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-28 18:16:34 +0100

    add some stupid stuff from css

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,19 @@
 function Homepage() {
   return (
     <>
-      <h1>Benvenuto nel nostro mondo.</h1>
-      <p className="welcome">
-        Ogni prodotto racconta una storia. Scopri la tua.
-      </p>
+      <div className="jumbo">
+        <img
+          className="jumbotron"
+          src="../public/rectangle_large_type_2_e5682b4be8efeffa646a42775430b2ed.webp"
+          alt=""
+        />
+        <div className="label-jumbo">
+          <h1>Benvenuto nel nostro mondo.</h1>
+          <p className="welcome">
+            Ogni prodotto racconta una storia. Scopri la tua.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
