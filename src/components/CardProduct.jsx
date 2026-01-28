4c1441dd301971c67669c1f7b1174169d163ba27
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-28 17:41:39 +0100

    add lil css

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -1,12 +1,11 @@
 function Card({ product }) {
-  const { image, title, description, price, rate } = product;
+  const { image, title, description, price } = product;
   return (
     <div className="card">
       <img src={image} alt="picture" />
       <h3>{title}</h3>
       <p>{description}</p>
       <div className="price">{price}</div>
-      <div className="rate">{rate}</div>
     </div>
   );
 }
