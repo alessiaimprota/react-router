b9648af5fbb7b0ed7e69aa1ee63150d617ebeb30
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-29 17:11:51 +0100

    add dynamic api call

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,10 +1,24 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const endpointDetail = "https://fakestoreapi.com/products";
+
 function ProductDetail() {
+  const [details, setDetails] = useState({});
   const { id } = useParams();
+  useEffect(() => {
+    axios
+      .get(`${endpointDetail}/${id}`)
+      .then((resp) => setDetails(resp.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <h1>ciaoooo</h1>
-      <p>prova {id}</p>
+      <p>prova {details.title}</p>
     </>
   );
 }
