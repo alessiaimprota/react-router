35e4b03993241a4e641681ecf59d65585ef9c642
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-29 16:14:23 +0100

    add detail page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import AboutUs from "./pages/AboutUs";
 import Products from "./pages/Products";
+import ProductDetail from "./components/ProductDetail";
 
 import DefaultLayout from "./layout/DefaultLayout";
 
@@ -14,9 +15,12 @@ export default function App() {
       <Routes>
         <Route element={<DefaultLayout />}>
           {/* importo qua poi le singole pagine e i loro percorsi di navigaizone */}
-          <Route path="/" Component={Homepage} />
-          <Route path="/products" Component={Products} />
-          <Route path="/aboutus" Component={AboutUs} />
+          <Route path="/" element={<Homepage />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/products">
+            <Route path="" element={<Products />} />
+            <Route path=":id" element={<ProductDetail />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/ProductDetail.jsx
@@ -0,0 +1,9 @@
+function ProductDetail() {
+  return (
+    <>
+      <h1>ciaoooo</h1>
+    </>
+  );
+}
+
+export default ProductDetail;
