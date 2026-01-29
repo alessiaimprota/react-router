5b32024524b86906b9c1b12c06537ab53559a064
alessiaimprota <alessiaimprota126@gmail.com>
2026-01-29 16:35:12 +0100

    check id with useParams

--- src/components/ProductDetail.jsx
+++ src/components/ProductDetail.jsx
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
 function ProductDetail() {
+  const { id } = useParams();
   return (
     <>
       <h1>ciaoooo</h1>
+      <p>prova {id}</p>
     </>
   );
 }
